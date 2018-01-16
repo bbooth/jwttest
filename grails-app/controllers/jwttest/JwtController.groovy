c930200b4b53445802b50c67f0d5484c60badac8
Bradley Booth <bradley.booth@gmail.com>
2018-01-15 18:41:07 -0700

    Update to use RSA keys

--- grails-app/controllers/jwttest/JwtController.groovy
+++ grails-app/controllers/jwttest/JwtController.groovy
@@ -1,24 +1,16 @@
 package jwttest
 
-import com.nimbusds.jose.*
-import com.nimbusds.jose.crypto.MACVerifier
-import com.nimbusds.jose.jwk.source.ImmutableSecret
+import com.nimbusds.jose.JWSAlgorithm
 import com.nimbusds.jose.jwk.source.JWKSource
-import com.nimbusds.jose.proc.BadJOSEException
-import com.nimbusds.jose.proc.JWEDecryptionKeySelector
-import com.nimbusds.jose.proc.JWEKeySelector
-import com.nimbusds.jose.util.Base64
-import com.nimbusds.jwt.EncryptedJWT
+import com.nimbusds.jose.jwk.source.RemoteJWKSet
+import com.nimbusds.jose.proc.JWSKeySelector
+import com.nimbusds.jose.proc.JWSVerificationKeySelector
+import com.nimbusds.jose.proc.SecurityContext
 import com.nimbusds.jwt.JWTClaimsSet
-import com.nimbusds.jwt.SignedJWT
 import com.nimbusds.jwt.proc.ConfigurableJWTProcessor
 import com.nimbusds.jwt.proc.DefaultJWTProcessor
 import org.jasig.cas.client.validation.Assertion
 import org.jasig.cas.client.validation.Cas20ServiceTicketValidator
-
-import javax.crypto.SecretKey
-import javax.crypto.spec.SecretKeySpec
-import java.text.ParseException
 
 class JwtController {
 
@@ -36,35 +28,22 @@ class JwtController {
     }
 
     private def decryptToken(String token) {
-        final MACVerifier verifier = new MACVerifier(grailsApplication.config.cas.signingKey as String)
-        final SecretKey decryptionKey = new SecretKeySpec(new Base64(grailsApplication.config.cas.encryptionKey as String).decode(), "AES")
-        final JWKSource decryptionKeySource = new ImmutableSecret(decryptionKey)
-        final JWEKeySelector encryptionKeySelector = new JWEDecryptionKeySelector(JWEAlgorithm.DIR, EncryptionMethod.A128CBC_HS256, decryptionKeySource)
-        final ConfigurableJWTProcessor jwtProcessor = new DefaultJWTProcessor<>()
-        jwtProcessor.setJWEKeySelector(encryptionKeySelector)
+        ConfigurableJWTProcessor jwtProcessor = new DefaultJWTProcessor()
+        JWKSource keySource = new RemoteJWKSet(new URL(grailsApplication.config.cas.jwksUrl))
+        JWSAlgorithm expectedJWSAlg = JWSAlgorithm.RS256
 
-        JWTClaimsSet claimsSet = null
-        try {
-            SignedJWT signedJWT = SignedJWT.parse(token)
-            final boolean validSignature = signedJWT.verify(verifier)
-            if (validSignature) {
-                Payload payload = signedJWT.getPayload()
-                String encryptedToken = new Base64(payload.toString()).decodeToString()
-                EncryptedJWT jwt = EncryptedJWT.parse(encryptedToken)
-                claimsSet = jwtProcessor.process(jwt, null)
-            }
-        } catch (final KeyException e) {
-            throw e
-        } catch (final JOSEException | ParseException | BadJOSEException e) {
-            log.debug("Exception while validating JWT token from CAS: $token", e)
-        }
+        JWSKeySelector keySelector = new JWSVerificationKeySelector(expectedJWSAlg, keySource)
+        jwtProcessor.setJWSKeySelector(keySelector)
+
+        SecurityContext ctx = null // optional context parameter, not required here
+        JWTClaimsSet claimsSet = jwtProcessor.process(token, ctx)
 
         return claimsSet.claims
     }
 
     private def validateServiceTicket(String token) {
         Cas20ServiceTicketValidator validator = new Cas20ServiceTicketValidator(grailsApplication.config.cas.serverUrlPrefix)
-        Assertion assertion = validator.validate(token, "http://localhost:8080/jwt")
+        Assertion assertion = validator.validate(token, grailsApplication.config.cas.serviceUrl)
 
         return assertion?.principal?.attributes
     }
